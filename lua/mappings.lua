5ac92c014ab06239666fa42d03873a9c7cffb9ea
Manel Pedro <86663328+manel-pedro@users.noreply.github.com>
2026-06-13 11:20:23 +0100

    lastest

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,12 +11,12 @@ map("n", "<C-S-Up>", "<cmd>resize +5<cr>", { desc = "Resize Window Up" })
 map("n", "<C-S-Down>", "<cmd>resize -5<cr>", { desc = "Resize Window Down" })
 map("n", "<C-S-Left>", "<cmd>vertical resize -5<cr>", { desc = "Resize Window Left" })
 map("n", "<C-S-Right>", "<cmd>vertical resize +5<cr>", { desc = "Resize Window Right" })
-
 map("n", "<A-Down>", "<cmd>m .+1<cr>==gl", { desc = "Move line down" })
 map("n", "<A-Up>", "<cmd>m .-2<cr>==gl", { desc = "Move line up" })
 
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "gh", "vim.lsp.buf.hover()")
 
 -- show error
 --
-map("n", "gl", "vim.diagnostic.open_float()")
+map("n", "gl", "lua vim.diagnostic.open_float()")
